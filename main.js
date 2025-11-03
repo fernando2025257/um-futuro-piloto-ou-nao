869d0098f7063858ca866d4d0e17abe9822a5de4
fernando2025257 <fernando.amaral.barbosa@escola.pr.gov.br>
2025-11-03 11:28:01 -0300

    k

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ avanca.forEach(button =>{
    button.addEventListener('click', function (){
     const atual=document.querySelector ('.ativo')
     const proximoPasso= 'passo-' + this .getAttribute('data-proximo')
-console.log(proximopaaso)
+
 atual.classList.remove('ativo')
   document.getelementbyld(proximoPasso).classlist.add('ativo')
 })
